88d6b422ae7d11639a7822b1d99d51312f681ae4
Dylan Miller <djm2@ualberta.ca>
2024-09-13 13:58:31 -0600

    Updated test for trap traj

--- tests/test_trapezoidal_profile.py
+++ tests/test_trapezoidal_profile.py
@@ -3,7 +3,17 @@ from diffusion_policy.common.trapezoidal_profile import trapezoidal_waypoints
 import matplotlib.pyplot as plt
 
 def test_trapezoidal_profile():
-    times, positions = trapezoidal_waypoints(np.array([0, 0, 0]), np.array([1, 2, 3]), 1, 1)
-    for i in range(3):
-        plt.plot(times[:, i], positions[:, i])
+    cur_pos = np.array([-0.00650116, -2.03248382,  0.03744754,  2.55714597, -0.088876,    0.64348122,
+      0.07058572,  0.,          0.        ])
+    goal_pos =  np.array([-3.83049790e-05,  8.52910535e-02, -6.12492730e-02,  2.77529342e+00,
+      3.49494592e-02,  1.31304435e-01,  4.03060575e-02,  0.00000000e+00,
+      0.00000000e+00])
+    v_max = np.array([0.5]*9)
+    a_max = np.array([0.5]*9)
+    times, positions = trapezoidal_waypoints(cur_pos, goal_pos, v_max, a_max, time_step=0.1)
+    for i in range(9):
+        print(f'Joint {i} positions: {positions[:, i]}')
+        plt.plot(times, positions[:, i])
+        plt.title(f'Joint {i}')
         plt.show()
+
