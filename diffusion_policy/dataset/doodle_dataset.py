2ddaab9bce7a8c5ac601717fc8c472e436d479cd
JustinValentine <JustinValentineCS@gmail.com>
2024-11-21 19:41:05 -0700

    Ploting Doodles

--- diffusion_policy/dataset/doodle_dataset.py
+++ diffusion_policy/dataset/doodle_dataset.py
@@ -233,13 +233,13 @@ class DoodleDataset(BaseDataset):
 def _convert_doodle_to_replay():
     replay_buffer = ReplayBuffer.create_empty_numpy()
 
-    with open('/home/odin/DiffusionPolicy/data/doodle/hard_class_index.json', 'r') as f:
+    with open('/home/odin/DiffusionPolicy/data/doodle/easy_class_index.json', 'r') as f:
         class_to_index = json.load(f)
 
     # Process the data
     max_trajectory_lenght = 64
 
-    with open('/home/odin/DiffusionPolicy/data/doodle/hard_data_train.csv', 'r') as f:
+    with open('/home/odin/DiffusionPolicy/data/doodle/easy_data_train.csv', 'r') as f:
         reader = csv.reader(f)
         i = 0
         for row in reader:
